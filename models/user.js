b79d789f915e93044005340a980adaf1cd4b3a25
Shnaider Danila <shnd3r@yandex.ru>
2023-09-28 17:48:56 +0700

    feat: Add models directory and user.js schema

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require('mongoose');
+
+const validator = require('validator');
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(v) {
+        return validator.isEmail(v);
+      },
+      message: 'Некорректный email адрес',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+});
+
+module.exports = model('user', userSchema);
