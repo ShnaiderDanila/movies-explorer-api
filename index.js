2fdb2fad99e59bb15b984d5ef978be2ee84dedc2
Shnaider Danila <shnd3r@yandex.ru>
2023-09-29 19:15:15 +0700

    feat: Connect the middleware function handleError in index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const handleError = require('./middlewares/errorHandler');
+
 const { PORT = 3000 } = process.env;
 
 const app = express();
@@ -9,5 +11,7 @@ mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(handleError);
 
 app.listen(PORT, () => {});
