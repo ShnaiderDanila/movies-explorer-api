87818ecbee16c14b1b414452a758d6229c89beed
Shnaider Danila <shnd3r@yandex.ru>
2023-10-02 18:26:54 +0700

    feat: Set up routing in movies.js

--- routes/movies.js
+++ routes/movies.js
@@ -1,3 +1,9 @@
 const moviesRouter = require('express').Router();
 
+const { getMovies, createMovie, deleteMovie } = require('../controllers/movies');
+
+moviesRouter.get('/', getMovies);
+moviesRouter.post('/', createMovie);
+moviesRouter.delete('/:_id', deleteMovie);
+
 module.exports = moviesRouter;
