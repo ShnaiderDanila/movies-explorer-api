2ed651c9424d9ff0ea0fddcfa572ae227da83e79
Shnaider Danila <shnd3r@yandex.ru>
2023-10-02 16:25:21 +0700

    fix: Fix connection to MongoDB server in index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
+mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
