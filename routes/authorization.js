a72c9a1119b4b9c3ffd705d07a7ea418008e771f
Shnaider Danila <shnd3r@yandex.ru>
2023-10-03 13:55:00 +0700

    feat: Set up a route for the signout controller in ./routes/authorization.js

--- routes/authorization.js
+++ routes/authorization.js
@@ -1,8 +1,9 @@
 const authorizationRouter = require('express').Router();
 
-const { signup, signin } = require('../controllers/users');
+const { signup, signin, signout } = require('../controllers/users');
 
 authorizationRouter.post('/signup', signup);
 authorizationRouter.post('/signin', signin);
+authorizationRouter.post('/signout', signout);
 
 module.exports = authorizationRouter;
