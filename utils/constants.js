1abb1716365f3379b62a63a9cb24f9ba8cf0921c
Shnaider Danila <shnd3r@yandex.ru>
2023-10-03 17:55:58 +0700

    feat: Add regular expressions to ./utils/constants.js

--- utils/constants.js
+++ utils/constants.js
@@ -1,2 +1,12 @@
 const CREATED_STATUS = 201;
-module.exports = { CREATED_STATUS };
+
+const regexURL = /^https?:\/\/(w{3}\.)?[-a-z0-9._~:?#[\]@!$&'()*+,;=/]+\.[a-z0-9]+\/?[-a-z0-9._~:?#[\]@!$&'()*+,;=/]*#?$/i;
+const regexNameRU = /[^a-z]/gi;
+const regexNameEN = /[^а-яё]/gi;
+
+module.exports = {
+  CREATED_STATUS,
+  regexURL,
+  regexNameRU,
+  regexNameEN,
+};
