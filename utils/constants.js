a1293f6d3106ba5b4e88f1fa95e8ebb57e1cf016
Shnaider Danila <shnd3r@yandex.ru>
2023-10-03 18:19:12 +0700

    fix: Remove global mode from regular expressions in ./utils/constants.js

--- utils/constants.js
+++ utils/constants.js
@@ -1,8 +1,8 @@
 const CREATED_STATUS = 201;
 
 const regexURL = /^https?:\/\/(w{3}\.)?[-a-z0-9._~:?#[\]@!$&'()*+,;=/]+\.[a-z0-9]+\/?[-a-z0-9._~:?#[\]@!$&'()*+,;=/]*#?$/i;
-const regexNameRU = /[^a-z]/gi;
-const regexNameEN = /[^а-яё]/gi;
+const regexNameRU = /[^a-z]/i;
+const regexNameEN = /[^а-яё]/i;
 
 module.exports = {
   CREATED_STATUS,
