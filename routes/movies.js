683a79989e0dd0ce0fcada759c69be472227e7f4
Shnaider Danila <shnd3r@yandex.ru>
2023-10-03 18:13:47 +0700

    feat: Add Joi Celebrate middleware functions to ./routes/movies.js

--- routes/movies.js
+++ routes/movies.js
@@ -1,9 +1,10 @@
 const moviesRouter = require('express').Router();
 
+const { createMovieValidation, deleteMovieValidation } = require('../middlewares/validation');
 const { getMovies, createMovie, deleteMovie } = require('../controllers/movies');
 
 moviesRouter.get('/', getMovies);
-moviesRouter.post('/', createMovie);
-moviesRouter.delete('/:_id', deleteMovie);
+moviesRouter.post('/', createMovieValidation, createMovie);
+moviesRouter.delete('/:_id', deleteMovieValidation, deleteMovie);
 
 module.exports = moviesRouter;
