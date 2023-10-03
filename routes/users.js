6d471304e3765c1920f31b1e52b4abc9a3798709
Shnaider Danila <shnd3r@yandex.ru>
2023-10-03 18:14:30 +0700

    feat: Add Joi Celebrate middleware functions to ./routes/users.js

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 const usersRouter = require('express').Router();
 
+const { updateUserInfoValidation } = require('../middlewares/validation');
 const { getUserInfo, updateUserInfo } = require('../controllers/users');
 
 usersRouter.get('/me', getUserInfo);
-usersRouter.patch('/me', updateUserInfo);
+usersRouter.patch('/me', updateUserInfoValidation, updateUserInfo);
 
 module.exports = usersRouter;
