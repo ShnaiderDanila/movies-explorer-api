aae6ea512e3fb2d13ace32e5427900b89e5a2494
Shnaider Danila <shnd3r@yandex.ru>
2023-10-03 15:51:30 +0700

    feat: Add validation functions using Joi Сelebrate to ./middlewares/validation.js

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,1 +1,53 @@
 const { celebrate, Joi } = require('celebrate');
+
+const signUpValidation = celebrate({
+  body: Joi.object.keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+    name: Joi.string().required().min(2).max(30),
+  }),
+});
+
+const signInValidation = celebrate({
+  body: Joi.object.keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+});
+
+const updateUserInfoValidation = celebrate({
+  body: Joi.object.keys({
+    email: Joi.string().required().email(),
+    name: Joi.string().required().min(2).max(30),
+  }),
+});
+
+const createMovieValidation = celebrate({
+  body: Joi.object.keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.string().required(),
+    description: Joi.string().required(),
+    image: Joi.string().required(),
+    trailerLink: Joi.string().required(),
+    thumbnail: Joi.string().required(),
+    movieId: Joi.number().required(),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+  }),
+});
+
+const deleteMovieValidation = celebrate({
+  params: Joi.object.keys({
+    _id: Joi.string().hex().length(24).required(),
+  }),
+});
+
+module.exports = {
+  signUpValidation,
+  signInValidation,
+  updateUserInfoValidation,
+  createMovieValidation,
+  deleteMovieValidation,
+};
