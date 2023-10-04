cc883b4d03308ec63d69708752bf7a667bddff58
Shnaider Danila <shnd3r@yandex.ru>
2023-10-04 14:31:03 +0700

    feat: Add processing of simple CORS requests to ./middlewares/cors.js

---
+++ middlewares/cors.js
@@ -0,0 +1,11 @@
+const { allowedCors, DEFAULT_ALLOWED_METHODS } = require('../utils/constants');
+
+const cors = (req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+};
+
+module.exports = cors;
