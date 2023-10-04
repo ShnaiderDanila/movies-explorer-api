27f94b623fe8dd836dd15d1bc80c69a40489950e
Shnaider Danila <shnd3r@yandex.ru>
2023-10-04 14:59:37 +0700

    feat: Add processing of CORS preflight requests to ./middlewares/cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,10 +2,17 @@ const { allowedCors, DEFAULT_ALLOWED_METHODS } = require('../utils/constants');
 
 const cors = (req, res, next) => {
   const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['Access-Control-Request-Headers'];
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
+    res.set('Access-Control-Allow-Origin', origin);
+    res.set('Access-Control-Allow-Credentials', true);
   }
+  if (method === 'OPTIONS') {
+    res.set('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.set('Access-Control-Allow-Headers', requestHeaders);
+  }
+  next();
 };
 
 module.exports = cors;
