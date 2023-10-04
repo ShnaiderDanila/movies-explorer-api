32b320401ad69c2a00f23ad7b620981a6e69d8a3
Shnaider Danila <shnd3r@yandex.ru>
2023-10-04 14:10:13 +0700

    feat: Add an array of CORS resolved domains to ./utils/constants.js

--- utils/constants.js
+++ utils/constants.js
@@ -4,9 +4,14 @@ const regexURL = /^https?:\/\/(w{3}\.)?[-a-z0-9._~:?#[\]@!$&'()*+,;=/]+\.[a-z0-9
 const regexNameRU = /^[^a-z]+$/i;
 const regexNameEN = /^[^а-яё]+$/i;
 
+const allowedCors = [
+
+];
+
 module.exports = {
   CREATED_STATUS,
   regexURL,
   regexNameRU,
   regexNameEN,
+  allowedCors,
 };
