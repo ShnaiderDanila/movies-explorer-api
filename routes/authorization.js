ea1c223aabc036435e35a03503912c70d1368107
Shnaider Danila <shnd3r@yandex.ru>
2023-10-07 13:54:08 +0700

    fix: Move the signout router from authorization.js to a separate signout.js file in ./routes

--- routes/authorization.js
+++ routes/authorization.js
@@ -1,10 +1,9 @@
 const authorizationRouter = require('express').Router();
 
 const { signUpValidation, signInValidation } = require('../middlewares/validation');
-const { signup, signin, signout } = require('../controllers/users');
+const { signup, signin } = require('../controllers/users');
 
 authorizationRouter.post('/signup', signUpValidation, signup);
 authorizationRouter.post('/signin', signInValidation, signin);
-authorizationRouter.post('/signout', signout);
 
 module.exports = authorizationRouter;
